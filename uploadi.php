889cb9e7724d076bbd3145732ef1f104d3c070c1
Edwin BabyMj <72539533+edwinMj13@users.noreply.github.com>
2021-01-24 11:40:48 +0530

    Update uploadi.php

--- uploadi.php
+++ uploadi.php
@@ -1,20 +1,24 @@
 <?php
-
+error_reporting(0);
 
     $name = $_POST["name"]; 
-     $image = $_POST["image"];
+    $image = $_POST["image"];
     $response = array();
- 
+
     $decodedImage = base64_decode("$image");
     $return = file_put_contents("imgess/".$name.".JPG", $decodedImage);
  
-    if($return !== false){
-        $response['success'] = 1;
-        $response['message'] = "Image Uploaded Successfully with Retrofit";
-    }else{
-        $response['success'] = 0;
-        $response['message'] = "Image Uploaded Failed";
-    }
+    if($return !== 0){
+
+          $response['success'] = "1";
+          $response['message'] = "Image Uploaded Successfully with Retrofit";
+    
+          }else{
+          
+          $response['success']="0";
+          $response['message'] = "Image Upload Failed";
+
+        }
  
     echo json_encode($response);
 ?>
