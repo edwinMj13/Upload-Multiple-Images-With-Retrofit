37cf28144367a9e3eef235902f698d7d1762e4dc
Edwin BabyMj <72539533+edwinMj13@users.noreply.github.com>
2021-02-17 12:59:27 +0530

    Create RecyclerviewClickInterface.java

---
+++ RecyclerviewClickInterface.java
@@ -0,0 +1,8 @@
+package com.example.imageuploading__retrofit;
+
+import android.view.View;
+
+public interface RecyclerviewClickInterface {
+    void onItemClicks(int position);
+    void onLongItemClicks(int position);
+}
